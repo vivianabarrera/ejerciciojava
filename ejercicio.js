5b051696bba881698eb0418176ae63a49e1f1930
vivianabarrera <60675746+vivianabarrera@users.noreply.github.com>
2020-07-15 21:06:53 -0500

    Add files via upload

---
+++ ejercicio.js
@@ -0,0 +1,24 @@
+var num1;
+var num2;
+var numero = 2;
+
+if (num1 > num2){
+    console.log ('num1 es mayor');
+}else {
+    console.log ('num2 es mayor');
+}
+
+ switch (numero) 
+ {
+     case 1:
+        console.log  ("num1 es mayor")
+         break;
+
+         case 2:
+            console.log  ("num2 es mayor")
+         break;
+ 
+     default:
+        console.log ("Esa opcion no existe");
+         break;
+ }
